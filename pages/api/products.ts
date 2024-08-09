7885d16ae0a392e32bd2d155465a8faca87a0112
hudson-sales <hudson-sales@live.com>
2024-08-09 11:44:03 -0300

    Excluindo a pasta products da pasta api

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -2,5 +2,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 import products from "../../database.json"
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    
     res.status(200).json(products)
 }
--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -14,42 +14,57 @@ export const getStaticProps: GetStaticProps = async (context) => {
   if (typeof id === 'string') {
     const product = await fetchProduct(id)
 
-    return { props: { product }, revalidate: 10  }
+    return {
+      props: { product },
+      revalidate: 10
+    }
   }
 
-  return { redirect: { destination: '/products', permanent: false } }
+  return {
+    redirect: {
+      destination: '/products',
+      permanent: false
+    }
+  }
 }
 
 
 export const getStaticPaths: GetStaticPaths = async () => {
-    const products = await fetchProducts()
-  
-    const paths = products.map(product => {
-      return { params: { id: product.id.toString() } }
-    })
-  
-    return { paths, fallback: false }
-  }
+  const products = await fetchProducts()
 
-  const Product: NextPage = (props: {
-    children?: ReactNode
-    product?: ProductType
-  }) => {
-    return (
-      <div>
-        <Head>
-          <title>{props.product!.name}</title>
-          <meta name="description" content={props.product!.description} />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-  
-        <Header />
-  
-        <Container className="mt-5">
-          <ProductDetails product={props.product!} />
-        </Container>
-      </div>
-    )
-  }
-  
+  const paths = products.map(product => {
+    return {
+      params: {
+        id: product.id.toString()
+      }
+    }
+  })
+
+  return { 
+     paths,
+     fallback: false
+    }
+}
+
+const Product: NextPage = (props: {
+  children?: ReactNode
+  product?: ProductType
+}) => {
+  return (
+    <div>
+      <Head>
+        <title>{props.product!.name}</title>
+        <meta name="description" content={props.product!.description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+
+      <Container className="mt-5">
+        <ProductDetails product={props.product!} />
+      </Container>
+    </div>
+  )
+}
+
 export default Product
